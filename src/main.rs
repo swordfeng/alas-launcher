0bb3ae4ee4203e2a702b5316561e08decc286068
Jianfeng Zhang <swordfeng123@gmail.com>
2025-05-18 20:58:46 -0700

    Resolve save config issue
    Ref: Tauri Issues 4633

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,18 @@ use std::{
     time::Duration,
 };
 
+use anyhow::{anyhow, Result};
+use base64::{prelude::BASE64_STANDARD, Engine};
 #[cfg(unix)]
 use command_group::Signal;
 use command_group::{CommandGroup, GroupChild};
-use tauri::{Manager, Url};
-
-use anyhow::{anyhow, Result};
 use serde_json::Value as JsonValue;
-use tracing::{info, warn};
+use tauri::{
+    webview::{PageLoadEvent, PageLoadPayload},
+    Manager, Url, WebviewWindow,
+};
+use tauri_plugin_dialog::{DialogExt, FilePath};
+use tracing::{error, info, warn};
 
 fn alas_repo_dir() -> PathBuf {
     // Always check if this is a typical same-folder portable distribution
@@ -201,6 +205,67 @@ impl Drop for ManagedBackend {
     }
 }
 
+static ALAS_LANUCHER_INJECTION_JS: &'static str = r#"
+if (!window.alas_launcher_injected) {
+    window.alas_launcher_injected = true;
+    (function () {
+        // const origSaveAs = window.saveAs;
+        window.saveAs = function (blob, filename) {
+            const reader = new FileReader();
+            reader.onload = async () => {
+                const data = reader.result.split(',')[1];
+                console.log(data);
+                window.__TAURI__.core.invoke('save_as', { filename, data });
+            };
+            reader.readAsDataURL(blob);
+        };
+    })();
+}
+"#;
+
+#[tauri::command]
+fn save_as(app_handle: tauri::AppHandle, filename: &str, data: &str) {
+    match BASE64_STANDARD.decode(data) {
+        Ok(decoded_data) => app_handle
+            .dialog()
+            .file()
+            .set_file_name(filename)
+            .save_file(move |path| {
+                let result: Result<()> = (move || {
+                    let file_path = path
+                        .as_ref()
+                        .and_then(FilePath::as_path)
+                        .ok_or_else(|| anyhow!("Invalid file path {:?}", &path))?;
+                    fs::write(file_path, &decoded_data)?;
+                    info!("Saved file to {:?}", file_path);
+                    Ok(())
+                })();
+                if let Err(e) = result {
+                    error!("Failed to save file: {:?}", e);
+                }
+            }),
+        Err(e) => {
+            error!("Failed to decode file content: {:?}", e);
+            return;
+        }
+    };
+}
+
+fn page_load_injector(webview: WebviewWindow, payload: PageLoadPayload<'_>) {
+    match payload.event() {
+        PageLoadEvent::Finished => {
+            info!(
+                "Injecting saveFile function to loaded page: {}",
+                payload.url()
+            );
+            if let Err(e) = webview.eval(ALAS_LANUCHER_INJECTION_JS) {
+                error!("Failed to inject JS to webview: {:?}", e);
+            }
+        }
+        _ => {}
+    }
+}
+
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     setup_environment()?;
@@ -221,11 +286,27 @@ fn main() -> Result<()> {
 
     info!("Starting Webview...");
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![save_as])
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
             let _ = app
                 .get_webview_window("main")
                 .and_then(|w| w.set_focus().ok());
         }))
+        .setup(|app| {
+            tauri::WebviewWindowBuilder::from_config(
+                app,
+                app.config()
+                    .app
+                    .windows
+                    .iter()
+                    .find(|w| w.label == "main")
+                    .unwrap(),
+            )?
+            .on_page_load(page_load_injector)
+            .build()?;
+            Ok(())
+        })
         .build(tauri::generate_context!())?
         .run(move |app_handle, event| {
             match event {
